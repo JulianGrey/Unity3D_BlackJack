5e7d8618ba51c2fbb453823a6cec6de179edce92
Julian Grey <julianrgrey@gmail.com>
2015-06-21 23:45:45 +0100

    Remove redundant code

--- GameInfoText.cs
+++ GameInfoText.cs
@@ -36,54 +36,6 @@ public class GameInfoText : MonoBehaviour {
                         text.text = "Hand surrendered!";
                     }
                 }
-                //else {
-                //    text.text = "";
-                //    if(gameplayScript.handOne.win) {
-                //        if(gameplayScript.handOne.isSplit) {
-                //            text.text = "Payout to player's first hand!";
-                //        }
-                //        else {
-                //            text.text = "Payout to player!";
-                //        }
-                //    }
-                //    if(gameplayScript.handTwo.win) {
-                //        if(gameplayScript.handOne.win) {
-                //            text.text += "\n";
-                //        }
-                //        text.text += "Payout to player's second hand!";
-                //    }
-                //    if(gameplayScript.dealerHand.win) {
-                //        if(gameplayScript.handOne.win || gameplayScript.handTwo.win) {
-                //            text.text += "\n";
-                //        }
-                //        text.text = "Payout to dealer!";
-                //    }
-                //    if(gameplayScript.handOne.bust) {
-                //        if(gameplayScript.handOne.isSplit) {
-                //            text.text += "PLAYER BUST (first hand)!";
-                //        }
-                //        else {
-                //            text.text = "PLAYER BUST!";
-                //        }
-                //    }
-                //    if(gameplayScript.handTwo.bust) {
-                //        text.text += "PLAYER BUST (second hand)!";
-                //    }
-                //    if(gameplayScript.dealerHand.bust) {
-                //        text.text = "DEALER BUST!";
-                //    }
-                //    if(gameplayScript.handOne.value == gameplayScript.dealerHand.value) {
-                //        if(gameplayScript.handOne.isSplit) {
-                //            text.text += "Push first hand";
-                //        }
-                //        else {
-                //            text.text += "Push";
-                //        }
-                //    }
-                //    if(gameplayScript.handTwo.value == gameplayScript.dealerHand.value) {
-                //        text.text += "Push second hand";
-                //    }
-                //}
                 if(gameplayScript.payout) {
                     int netWinnings = gameplayScript.totalChips - gameplayScript.initTotalChips;
                     text.text = "Net winnings: ";
